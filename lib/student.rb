7654a3d73d7f0996a115fc6d9605a058edc125e8
Sara Walker <sarawall1187@gmail.com>
2019-02-12 22:50:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,58 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+ attr_accessor :name, :grade, :id
 
 
+ def initialize(name, grade, id = nil)
+   @id = id
+   @name = name
+   @grade = grade
+ end
+
+ def self.create_table
+   sql = <<-SQL
+   CREATE TABLE IF NOT EXISTS students (
+     id INTEGER PRIMARY KEY,
+     name TEXT,
+     grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+ end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+ end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+   new_student = Student.new(name, grade)
+   new_student.save
+   new_student
+  end
+
+  def self.new_from_db(row)
+    new_student = Student.new(id, name, grade)
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+
+    new_student
+  end
 end
