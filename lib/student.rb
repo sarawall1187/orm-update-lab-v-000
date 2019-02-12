e52c215fdcef379c4ea173100d68d0aba60dbe13
Sara Walker <sarawall1187@gmail.com>
2019-02-12 22:59:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,23 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(id, name, grade)
+    new_student = Student.new(row[0], row[1], row[2])
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
-
     new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE name = ?
+     LIMIT 1
+     SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
   end
 end
